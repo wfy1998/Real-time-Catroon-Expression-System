a8d70585a73f441760420cc9187d6b371951b28d
wfy1998 <wang.f.ac@m.titech.ac.jp>
2023-06-09 17:29:04 +0900

    data_record

--- data_record.py
+++ data_record.py
@@ -1,18 +1,20 @@
-# record the landmark data.
-from asyncore import write
-import imp
-from tkinter import image_names
-from traceback import print_tb
-import cv2
-from matplotlib.pyplot import axis
-import mediapipe as mp
-import numpy as np
-import time
-import csv
-import keyboard
 import os
+import cv2
+import mediapipe as mp
+import time
+import keyboard
+import numpy as np
+import csv
+from argparse import ArgumentParser
+from core.videosource import WebcamSource
+from core.face_geometry import (
+    PCF,
+    get_metric_landmarks,
+    procrustes_landmark_basis,
+)
 
 
+# mediapipe 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
@@ -20,86 +22,95 @@ drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
 
 
-t_end = time.time() + 2
 
-datacheck = 0
-stop_time = time.time() + 20
-data = []
-size_of_landmarks = 478
-data_dimentions = 3
-np_arr = np.empty((0,size_of_landmarks,data_dimentions))
-image_counter = 0
-datas = []
-file_name_array = []
-rows = []
-origin_x = 0
-origin_y = 0
-origin_z = 0
+# data normalization
+landmark_number = 468
+points_idx = [33, 263, 61, 291, 199]
+points_idx = points_idx + [key for (key, val) in procrustes_landmark_basis]
+points_idx = list(set(points_idx))
+points_idx.sort()
 
-# flags
-train = 1
-store_method = 1
-expression_ID = 1
-show_landmarks = 1
-relative_coordinates = 1
-if relative_coordinates == 1:
-    landmark_store_file_name = "Relative_coordinates_data.csv"
-else:
-    landmark_store_file_name = "simple_data.csv"
+# uncomment next line to use all points for PnP algorithm
+points_idx = list(range(0,landmark_number)); points_idx[0:2] = points_idx[0:2:-1]
+points_idx = list(range(0,landmark_number))
+frame_height, frame_width, channels = (720, 1280, 3)
+# pseudo camera internals
+focal_length = frame_width
+center = (frame_width / 2, frame_height / 2)
+camera_matrix = np.array(
+    [[focal_length, 0, center[0]], [0, focal_length, center[1]], [0, 0, 1]],
+    dtype="double",
+)
+dist_coeff = np.zeros((4, 1))
 
 
-#get last image ID
-if (train == 1):
-# print(os.stat("data/train/expression_{}/file_name_list.csv".format(expression_ID)).st_size)
-    if os.stat("data/train/expression_{}/file_name_list.csv".format(expression_ID)).st_size != 0:
-        with open("data/train/expression_{}/file_name_list.csv".format(expression_ID), "r") as file:
-            print("open file")
-            last_line = file.readlines()[-2]
-            # print(last_line)
-            image_counter = int(list(filter(str.isdigit, last_line))[0]) + 1
-            # print(image_counter)
-            file.close
-else:
-    if os.stat("data/test/expression_{}/file_name_list.csv".format(expression_ID)).st_size != 0:
-        with open("data/test/expression_{}/file_name_list.csv".format(expression_ID), "r") as file:
-            print("open file")
-            last_line = file.readlines()[-2]
-            # print(last_line)
-            image_counter = int(list(filter(str.isdigit, last_line))[0]) + 1
-            # print(image_counter)
-            file.close
 
-if(train == 1):
-    f = open('data/train/expression_{}/landmark_data.csv'.format(expression_ID), 'a')
-    file_name_list = open('data/train/expression_{}/file_name_list.csv'.format(expression_ID), 'a')
-else:
-    f = open('data/test/expression_{}/landmark_data.csv'.format(expression_ID), 'a')
-    file_name_list = open('data/test/expression_{}/file_name_list.csv'.format(expression_ID), 'a')
 
-writer = csv.writer(f)
-file_name_writer = csv.writer(file_name_list)
+def main():
+  # par setting
+  expressionID = 0 # expression ID
+  user_name = '' # user name
+  file_name_3D = '' # file name that store 2D data
+  file_name_3D = '' # file name that store 3D data
+  frame_step = 28 # vedio record length. default is 28 frame.
+  floder_name = '' # floder that store user's data
+  
+  # ask user enter info
+  user_name = input("please enter your name: ")
+  expressionID = input("Please enter the expression ID you want to record: ")
+  
+  
+  # set file name that store 3D and 2D data
+  folder_name = user_name
+  # create folder if first time record
+  if not os.path.exists("data/{}".format(folder_name)):
+    os.mkdir("data/{}".format(folder_name))
+  
 
-with mp_face_mesh.FaceMesh(
-    max_num_faces=1,
-    refine_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as face_mesh:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+  if args.data_normalization:
+    threeDfile = "3D_time_data_{}.csv".format(user_name) 
+    twoDfile = "2D_time_data_{}.csv".format(user_name)
+  else:
+    threeDfile = "3D_time_data_c.csv".format(user_name)
+    twoDfile = "2D_time_data_c.csv".format(user_name)
+  file_3d = open('data/{}/{}'.format(user_name, threeDfile), mode='a', newline='')
+  file_2d = open('data/{}/{}'.format(user_name, twoDfile), mode='a', newline='')
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = face_mesh.process(image)
-    record_image = image
 
-    if show_landmarks:
-        # Draw the face mesh annotations on the image.
+  # record start
+  flag2= 0
+  rows_3d = []
+  rows_2d = []
+  flag2= 0
+  frame_count = 0
+
+  pcf = PCF(
+      near=1,
+      far=10000,
+      frame_height=frame_height,
+      frame_width=frame_width,
+      fy=camera_matrix[1, 1],
+  )
+
+  with mp_face_mesh.FaceMesh(
+      max_num_faces=1,
+      refine_landmarks=True,
+      min_detection_confidence=0.5,
+      min_tracking_confidence=0.5) as face_mesh:
+    while cap.isOpened():
+      success, image = cap.read()
+      if not success:
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue 
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      image2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = face_mesh.process(image)
+
+      # Draw the face mesh annotations on the image.
       image.flags.writeable = True
       image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
       if results.multi_face_landmarks:
@@ -126,78 +137,95 @@ with mp_face_mesh.FaceMesh(
               connection_drawing_spec=mp_drawing_styles
               .get_default_face_mesh_iris_connections_style())
 
-    # get results.multi_face_landmarks
-    if keyboard.is_pressed('space'):
+      # store landmark video
+      if args.press_key:
+        if keyboard.is_pressed('space'):
+          print("start record data")
+          start_time = time.time()
+          flag2 = 1
+          time.sleep(0.1)
+      else:
+        flag2 = 1
 
-        if store_method == 1:
-        # method two(put all data in one csv file)
-            face = results.multi_face_landmarks[0].landmark
-            if relative_coordinates == 1:
-                origin_x = face[0].x
-                origin_y = face[0].y
-                origin_z = face[0].z
-                face_row = list(np.array([[origin_x - landmark.x, origin_y -  landmark.y, origin_z - landmark.z] for landmark in face]).flatten())
-            else:
-                face_row = list(np.array([[landmark.x, landmark.y, landmark.z] for landmark in face]).flatten())
-            row = face_row 
-            row.insert(0, expression_ID)
-            rows.append(row)
+      if flag2 == 1:
+        if frame_count < frame_step:
+          if args.data_normalization:
+            landmarks = np.array([(lm.x, lm.y, lm.z) for lm in results.multi_face_landmarks[0].landmark ])
+            landmarks = landmarks.T
+            landmarks = landmarks[:, :landmark_number]
+            metric_landmarks, pose_transform_mat = get_metric_landmarks(
+                      landmarks.copy(), pcf
+                  )
+            model_points = metric_landmarks[0:3, points_idx].T
+          
+            origin_x = model_points[0][0]
+            origin_y = model_points[0][1]
+            origin_z = model_points[0][2]
+            face_row_3d = list(np.array([[origin_x - landmark[0], origin_y -  landmark[1], origin_z - landmark[2]] for landmark in model_points]).flatten())
+            face_row_2d = list(np.array([[origin_x - landmark[0], origin_y -  landmark[1]] for landmark in model_points]).flatten())
+          else:
+            face = results.multi_face_landmarks[0].landmark 
+            origin_x = face[0].x
+            origin_y = face[0].y
+            origin_z = face[0].z
+            face_row_3d = list(np.array([[origin_x - landmark.x, origin_y -  landmark.y, origin_z - landmark.z] for landmark in face]).flatten())
+            face_row_2d = list(np.array([[origin_x - landmark.x, origin_y -  landmark.y] for landmark in face]).flatten())
 
+          rows_3d.append(face_row_3d)
+          rows_2d.append(face_row_2d)
+          frame_count += 1   
+        else:
+          flag2 = 0
+          rows_3d.insert(0,expressionID)
+          rows_2d.insert(0,expressionID)
+          # write 3D data to file
+          if args.record_data:
+            csv_writer1 =  csv.writer(file_3d, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer1.writerow(rows_3d)
 
-        elif store_method == 0:
-            for index in range(len(results.multi_face_landmarks[0].landmark)):
-                data.append([results.multi_face_landmarks[0].landmark[index].x,results.multi_face_landmarks[0].landmark[index].y,results.multi_face_landmarks[0].landmark[index].z])
-            #writer.writerow(datas)
-            if train == 1:
-                cv2.imwrite("data/train/expression_{}/images/".format(expression_ID) + image_names,record_image)
-            else:
-                cv2.imwrite("data/test/expression_{}/images/".format(expression_ID) + image_names,record_image)
-            np_arr = np.append(np_arr, np.array([data]),axis = 0)
-            datas.append(data)
-            data = []
-        image_names = "image_{}.png".format(image_counter)
-        file_name_array.append([image_names])
-        image_counter += 1
-        print("store data")
-        time.sleep(1)
-    
-    # save method two
-      # k = cv2.waitKey(1)
-      # if k%256 == 32:
-      #   for index in range(len(results.multi_face_landmarks[0].landmark)):
-      #     datas.append([results.multi_face_landmarks[0].landmark[index].x,results.multi_face_landmarks[0].landmark[index].y,results.multi_face_landmarks[0].landmark[index].z])
-      #   writer.writerow(datas)
-      #   np_arr = np.append(np_arr, np.array([datas]),axis = 0)
-      #   datas = []   
-      #   print("data save")
+            csv_writer2 =  csv.writer(file_2d, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer2.writerow(rows_2d)
+            print("record finish, size is: " + str(len(rows_2d)) + " and: " + str(len(rows_2d[1])))
 
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-    if keyboard.is_pressed('q'):
+          else:
+            print(rows_2d[1])
+            print("record finish, size is: " + str(len(rows_2d)) + " and: " + str(len(rows_2d[1])))
+            print("record fail")
+          rows_3d = []
+          rows_2d = []
+          frame_count =0
+      # Flip the image horizontally for a selfie-view display.
+      cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
+      cv2.imshow('second window', cv2.flip(image2,1))
+      if cv2.waitKey(1) & 0xFF == 'q':
         break
+      if keyboard.is_pressed('q'):
+        break
+  file_3d.close()
+  file_2d.close()
+  cap.release()
 
-print("The expression ID is: " + str(expression_ID))
-result = input("if you want to store those data:")
-if result.find('y') != -1:
-    if store_method == 1:
-        with open('data/{}'.format(landmark_store_file_name), mode='a', newline='') as f:
-            for row in rows:
-                csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow(row)
-            for name in file_name_array:
-                file_name_writer.writerow(name)
-    elif store_method == 0:
-        for record_data in datas:
-            writer.writerow(record_data)
-        for name in file_name_array:
-            file_name_writer.writerow(name)
-    print("store all data")
-else:
-    print("delete data")
+if __name__ == "__main__":
 
-f.close()
-file_name_list.close()
-cap.release()
+    parser = ArgumentParser()
 
+    parser.add_argument("--data_normalization",action="store_true",
+                        help="Normalize the record data",
+                        default=True)
+
+    parser.add_argument("--press_key",action="store_true",
+                        help="press 'space' to start the data record",
+                        default=True)
+
+    parser.add_argument("--record_data",action="store_true",
+                        help="Store landmark data to database",
+                        default=False)
+
+    parser.add_argument("--debug", action="store_true",
+                        help="showing raw values of detection in the terminal",
+                        default=False)
+
+    args = parser.parse_args()
+
+    # demo code
+    main()
